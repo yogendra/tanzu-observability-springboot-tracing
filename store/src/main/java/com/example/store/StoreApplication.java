e18fb31c191eacecf1e785fe2612be26e3f6c961
Yogendra Rampuria (Yogi) <yrampuria@vmware.com>
2021-06-24 09:22:00 +0800

    organize imports

--- store/src/main/java/com/example/store/StoreApplication.java
+++ store/src/main/java/com/example/store/StoreApplication.java
@@ -1,25 +1,22 @@
 package com.example.store;
 
-import lombok.extern.slf4j.Slf4j;
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
-
-
-
-import reactor.core.publisher.Mono;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.time.Duration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Slf4j
